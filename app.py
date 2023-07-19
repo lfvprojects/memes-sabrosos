52c63ee70b6f3d151b2de83265a875e1e14c187d
lfvprojects <126591854+lfvprojects@users.noreply.github.com>
2023-07-18 22:07:40 -0500

    Main app file structure

---
+++ app.py
@@ -0,0 +1,34 @@
+"""""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hola desde la aplicación Flask!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+"""
+#!/bin/python
+
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+def get_meme():
+    url = "https://meme-api.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    meme_lrg = response["preview"] [-2]
+    subreddit = response["subreddit"]
+    return meme_lrg, subreddit
+
+@app.route("/")
+def index():
+    meme_img, subreddit = get_meme()
+    return render_template("index.html", meme_img=meme_img, subreddit=subreddit)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
