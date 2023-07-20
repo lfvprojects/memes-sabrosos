d3ea044c750860349edfe6693e55720a02c20e33
jaimesoad <jaime.ac1106@gmail.com>
2023-07-20 11:24:48 -0500

    comentarios innecesarios eliminados en app.py

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-"""""
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hola desde la aplicación Flask!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-"""
-#!/bin/python
+# !/bin/python
 
 from flask import Flask, render_template
 import requests
